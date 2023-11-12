1e20aff4b87b019fb759b2a27a04b0bea18f2c81
Marco Vogt <marco.vogt@unibas.ch>
2023-11-12 11:45:40 +0100

    Improve verify hashes script

--- helpers/verify_hashes.py
+++ helpers/verify_hashes.py
@@ -1,19 +1,35 @@
 import os
+import sys
 
-def verify_hashes_in_directory(hash_file_path, directory_path):
+def verify_submissions(hash_file_path, directory_path):
     """
     Check whether the hashes in the file name of the files in the specified folder
     appear in the hashes.txt file.
     """
     # Read hashes file
     with open(hash_file_path, 'r', encoding='utf-8') as file:
-        hashes = {line.strip() for line in file}
+        valid_hashes = {line.strip() for line in file}
 
-    # Check each file in the specified folder
+     # Check each file in the specified folder
     for filename in os.listdir(directory_path):
+        # Ignore files starting with a dot (e.g. .gitkeep)
+        if filename.startswith('.'):
+            continue
+
         file_hash = filename.split('.')[0]  # ignore file extensions
-        if file_hash not in hashes:
-            return 1  # terminate script if there is an unknown hash
+        if file_hash not in valid_hashes:
+            print(f"Found invalid submission: {filename}")
+            return 1 
 
-    return 0 
+    print("All submissions are valid.")
+    return 0  # Alle Hashes stimmen überein
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python verify_hashes_in_directory.py [hash_file_path] [directory_path]")
+        sys.exit(1)
+
+    hash_file_path = sys.argv[1]
+    directory_path = sys.argv[2]
+    result = verify_submissions(hash_file_path, directory_path)
+    sys.exit(result)
